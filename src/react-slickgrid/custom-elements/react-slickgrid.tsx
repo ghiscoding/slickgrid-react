70916a080608e3e04a136df6d6934aa81a726d7e
Devin Garner <devingarner@degasolutions.com>
2021-09-19 03:50:19 -0600

    commit message

--- src/react-slickgrid/custom-elements/react-slickgrid.tsx
+++ src/react-slickgrid/custom-elements/react-slickgrid.tsx
@@ -1,5 +1,6 @@
 // import 3rd party vendor libs
 // also only import jQueryUI necessary widget (note autocomplete & slider are imported in their respective editors/filters)
+import * as $ from 'jquery';
 import 'jquery-ui/ui/widgets/draggable';
 import 'jquery-ui/ui/widgets/droppable';
 import 'jquery-ui/ui/widgets/sortable';
@@ -9,15 +10,38 @@ import 'slickgrid/slick.core';
 import 'slickgrid/slick.dataview';
 import 'slickgrid/slick.grid';
 import 'slickgrid/slick.groupitemmetadataprovider';
-import React from 'react';
+import React, { useState } from 'react';
 import { GlobalEventPubSubService } from '../services/singletons';
 
 interface Props { }
 
 export const ReactSlickgridCustomElement: React.FC<Props> = ({ }) => {
   const _eventPubSubService = GlobalEventPubSubService;
+  const [gridId, setGridId] = useState(1);
 
   return (
-    <span>7</span>
+    <div id={`slickGridContainer-{gridId}`} className="grid-pane">
+      {/* <!-- Header slot if you need to create a complex custom header --> */}
+      <slot name="slickgrid-header"></slot>
+
+      <div id={`${gridId}`} className="slickgrid-container" style={{ width: '100%' }} onBlur={$event => commitEdit($event.target)}>
+      </div>
+
+      {/* <!-- Pagination section under the grid-- > */}
+      {/*
+      {showPagination &&
+        <slick-pagination id={`slickPagingContainer-${gridId}`} grid-options={gridOptions} pagination-service={paginationService}>
+        </slick-pagination>
+      }
+      */}
+
+      {/* <!--Footer slot if you need to create a complex custom footer-- > */}
+      <slot name="slickgrid-footer"></slot>
+    </div >
   );
 };
+
+function commitEdit(target: any): React.FocusEventHandler<HTMLDivElement> {
+  throw new Error('Function not implemented.');
+}
+
