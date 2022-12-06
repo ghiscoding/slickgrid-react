5f9fdea4f9e51137a935fa16e9465f317c2495c4
ghiscoding <gbeaulac@gmail.com>
2022-12-06 00:20:22 -0500

    chore: fix JS syntax for Cypress config files

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -21,39 +21,18 @@
 // Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
 //
 //
-// -- This is will overwrite an existing command --
-import '@4tw/cypress-drag-drop';
-
+// -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-Cypress.Commands.add('triggerHover', (elements) => {
-  console.log(elements)
-  elements.each((index, element) => {
-    fireEvent(element, 'mouseover');
-  });
+import '@4tw/cypress-drag-drop';
+import { convertPosition } from './common';
 
-  function fireEvent(element, event) {
-    if (element.fireEvent) {
-      element.fireEvent('on' + event);
-    } else {
-      var evObj = document.createEvent('Events');
+// convert position like 'topLeft' to the object { x: 'left|right', y: 'top|bottom' }
+Cypress.Commands.add("convertPosition", (viewport = 'topLeft') => cy.wrap(convertPosition(viewport)));
 
-      evObj.initEvent(event, true, false);
+Cypress.Commands.add("getCell", (row, col, viewport = 'topLeft', { parentSelector = '', rowHeight = 35 } = {}) => {
+  const position = convertPosition(viewport);
+  const canvasSelectorX = position.x ? `.grid-canvas-${position.x}` : '';
+  const canvasSelectorY = position.y ? `.grid-canvas-${position.y}` : '';
 
-      element.dispatchEvent(evObj);
-    }
-  }
-});
-
-let LOCAL_STORAGE_MEMORY = {};
-
-Cypress.Commands.add("saveLocalStorage", () => {
-  Object.keys(localStorage).forEach(key => {
-    LOCAL_STORAGE_MEMORY[key] = localStorage[key];
-  });
-});
-
-Cypress.Commands.add("restoreLocalStorage", () => {
-  Object.keys(LOCAL_STORAGE_MEMORY).forEach(key => {
-    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
-  });
+  return cy.get(`${parentSelector} ${canvasSelectorX}${canvasSelectorY} [style="top:${row * rowHeight}px"] > .slick-cell:nth(${col})`);
 });
--- test/cypress/support/index.js
+++ test/cypress/support/index.js
@@ -14,10 +14,26 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('window:before:load', function (window) {
+  const original = window.EventTarget.prototype.addEventListener;
+
+  window.EventTarget.prototype.addEventListener = function () {
+    if (arguments && arguments[0] === 'beforeunload') {
+      return;
+    }
+    return original.apply(this, arguments);
+  }
+
+  Object.defineProperty(window, 'onbeforeunload', {
+    get: function () { },
+    set: function () { }
+  });
+});
 
 Cypress.Cookies.defaults({
   preserve: 'serve-mode',
