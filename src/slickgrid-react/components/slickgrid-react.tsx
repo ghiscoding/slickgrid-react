ghiscoding/slickgrid-react

commit 14703dfd087560b779f157c98e288aa39943bc56

fix: registered external resouces should keep singleton ref (#262)
* fix: registered external resouces should keep singleton ref

src/slickgrid-react/components/slickgrid-react.tsx
@@ -275,6 +275,14 @@ export class SlickgridReact extends React.Component<SlickgridReactProps, State> 
     // we only want to do this check once in the constructor
     this._hideHeaderRowAfterPageLoad = (props.gridOptions?.showHeaderRow === false);
 
+    // save resource refs to register before the grid options are merged and possibly deep copied
+    // since a deep copy of grid options would lose original resource refs but we want to keep them as singleton
+    this._registeredResources = props.gridOptions?.externalResources || props.gridOptions?.registerExternalResources || [];
+    /* istanbul ignore if */
+    if (props.gridOptions?.registerExternalResources) {
+      console.warn('[Slickgrid-Universal] Please note that the grid option `registerExternalResources` was deprecated, please use `externalResources` instead.');
+    }
+
     this._gridOptions = this.mergeGridOptions(props.gridOptions || {});
 
     // initialize and assign all Service Dependencies
@@ -382,6 +390,14 @@ export class SlickgridReact extends React.Component<SlickgridReactProps, State> 
           );
         }
       }
+    }
+
+    // save resource refs to register before the grid options are merged and possibly deep copied
+    // since a deep copy of grid options would lose original resource refs but we want to keep them as singleton
+    this._registeredResources = this.gridOptions?.externalResources || this.gridOptions?.registerExternalResources || [];
+    /* istanbul ignore if */
+    if (this.gridOptions?.registerExternalResources) {
+      console.warn('[Slickgrid-React] Please note that the grid option `registerExternalResources` was deprecated and will be removed in next major, please use `externalResources` instead.');
     }
 
     this.initialization(this._eventHandler);
@@ -622,15 +638,7 @@ export class SlickgridReact extends React.Component<SlickgridReactProps, State> 
     this.serviceList = [];
 
     // dispose all registered external resources
-    if (Array.isArray(this._registeredResources)) {
-      while (this._registeredResources.length > 0) {
-        const resource = this._registeredResources.pop();
-        if (resource?.dispose) {
-          resource.dispose();
-        }
-      }
-      this._registeredResources = [];
-    }
+    this.disposeExternalResources();
 
     // dispose the Components
     this.slickEmptyWarning?.dispose();
@@ -676,6 +684,18 @@ export class SlickgridReact extends React.Component<SlickgridReactProps, State> 
 
   dispose(shouldEmptyDomElementContainer = false) {
     this.componentWillUnmount(shouldEmptyDomElementContainer);
+  }
+
+  disposeExternalResources() {
+    if (Array.isArray(this._registeredResources)) {
+      while (this._registeredResources.length > 0) {
+        const res = this._registeredResources.pop();
+        if (res?.dispose) {
+          res.dispose();
+        }
+      }
+    }
+    this._registeredResources = [];
   }
 
   componentDidUpdate(prevProps: SlickgridReactProps) {
@@ -1405,16 +1425,37 @@ export class SlickgridReact extends React.Component<SlickgridReactProps, State> 
     return options;
   }
 
+  /** Add a register a new external resource, user could also optional dispose all previous resources before pushing any new resources to the resources array list. */
+  registerExternalResources(resources: ExternalResource[], disposePreviousResources = false) {
+    if (disposePreviousResources) {
+      this.disposeExternalResources();
+    }
+    resources.forEach(res => this._registeredResources.push(res));
+    this.initializeExternalResources(resources);
+  }
+
+  resetExternalResources() {
+    this._registeredResources = [];
+  }
+
   /** Pre-Register any Resource that don't require SlickGrid to be instantiated (for example RxJS Resource) */
   protected preRegisterResources() {
-    this._registeredResources = this.gridOptions.registerExternalResources || [];
-
     // bind & initialize all Components/Services that were tagged as enabled
     // register all services by executing their init method and providing them with the Grid object
     if (Array.isArray(this._registeredResources)) {
       for (const resource of this._registeredResources) {
         if (resource?.className === 'RxJsResource') {
           this.registerRxJsResource(resource as RxJsFacade);
+        }
+      }
+    }
+  }
+
+  protected initializeExternalResources(resources: ExternalResource[]) {
+    if (Array.isArray(resources)) {
+      for (const resource of resources) {
+        if (this.grid && typeof resource.init === 'function') {
+          resource.init(this.grid, this.props.containerService);
         }
       }
     }
@@ -1450,13 +1491,7 @@ export class SlickgridReact extends React.Component<SlickgridReactProps, State> 
 
     // bind & initialize all Components/Services that were tagged as enabled
     // register all services by executing their init method and providing them with the Grid object
-    if (Array.isArray(this._registeredResources)) {
-      for (const resource of this._registeredResources) {
-        if (this.grid && typeof resource.init === 'function') {
-          resource.init(this.grid, this.props.containerService);
-        }
-      }
-    }
+    this.initializeExternalResources(this._registeredResources);
   }
 
   /** Register the RxJS Resource in all necessary services which uses */

src/examples/slickgrid/Example12.tsx
@@ -192,7 +192,7 @@ class Example12 extends React.Component<Props, State> {
         sanitizeDataExport: true
       },
       excelExportOptions: { exportWithFormatter: true, sanitizeDataExport: true },
-      registerExternalResources: [this.excelExportService, this.textExportService],
+      externalResources: [this.excelExportService, this.textExportService],
     };
 
     this.setState((state: State) => ({

src/examples/slickgrid/Example13.tsx
@@ -202,7 +202,7 @@ export default class Example13 extends React.Component<Props, State> {
       enableTextExport: true,
       excelExportOptions: { sanitizeDataExport: true },
       textExportOptions: { sanitizeDataExport: true },
-      registerExternalResources: [this.excelExportService, this.textExportService],
+      externalResources: [this.excelExportService, this.textExportService],
     };
 
     this.setState((state: State) => ({

src/examples/slickgrid/Example14.tsx
@@ -70,7 +70,7 @@ export default class Example14 extends React.Component {
       excelExportOptions: {
         exportWithFormatter: false
       },
-      registerExternalResources: [new ExcelExportService()],
+      externalResources: [new ExcelExportService()],
       explicitInitialization: true,
       colspanCallback: this.renderDifferentColspan
     };
@@ -101,7 +101,7 @@ export default class Example14 extends React.Component {
       excelExportOptions: {
         exportWithFormatter: false
       },
-      registerExternalResources: [new ExcelExportService()],
+      externalResources: [new ExcelExportService()],
       gridMenu: { hideClearFrozenColumnsCommand: false },
       headerMenu: { hideFreezeColumnsCommand: false }
     };

src/examples/slickgrid/Example18.tsx
@@ -250,7 +250,7 @@ export default class Example18 extends React.Component<Props, State> {
       enableExcelExport: true,
       excelExportOptions: { sanitizeDataExport: true },
       textExportOptions: { sanitizeDataExport: true },
-      registerExternalResources: [this.excelExportService, this.textExportService],
+      externalResources: [this.excelExportService, this.textExportService],
     };
 
     this.setState((state: State) => ({

src/examples/slickgrid/Example23.tsx
@@ -214,7 +214,7 @@ class Example23 extends React.Component<Props, State> {
           { columnId: 'duration', direction: 'ASC' },
         ],
       },
-      registerExternalResources: [new SlickCustomTooltip(), new ExcelExportService()],
+      externalResources: [new SlickCustomTooltip(), new ExcelExportService()],
     };
 
     this.setState((state: State, props: Props) => ({

src/examples/slickgrid/Example24.tsx
@@ -304,7 +304,7 @@ class Example24 extends React.Component<Props, State> {
         // you can customize how the header titles will be styled (defaults to Bold)
         columnHeaderStyle: { font: { bold: true, italic: true } }
       },
-      registerExternalResources: [new ExcelExportService()],
+      externalResources: [new ExcelExportService()],
       i18n: i18next,
 
       enableContextMenu: true,

src/examples/slickgrid/Example27.tsx
@@ -152,7 +152,7 @@ export default class Example27 extends React.Component<Props, State> {
       },
       enableExcelExport: true,
       excelExportOptions: { exportWithFormatter: true, sanitizeDataExport: true },
-      registerExternalResources: [new ExcelExportService()],
+      externalResources: [new ExcelExportService()],
     };
 
     this.setState((state: State) => ({

commit dcf01e42c7edcc84db53085df2a48ace9caefeb3

fix: regression from PR #262, read external resource when required - PR #262 can potentially cause a regression with external resource registered globally

src/slickgrid-react/components/slickgrid-react.tsx
@@ -262,10 +262,6 @@ export class SlickgridReact<TData = any> extends React.Component<SlickgridReactP
     // we only want to do this check once in the constructor
     this._hideHeaderRowAfterPageLoad = (props.gridOptions?.showHeaderRow === false);
 
-    // save resource refs to register before the grid options are merged and possibly deep copied
-    // since a deep copy of grid options would lose original resource refs but we want to keep them as singleton
-    this._registeredResources = props.gridOptions?.externalResources || [];
-
     this._gridOptions = this.mergeGridOptions(props.gridOptions || {});
 
     // initialize and assign all Service Dependencies
@@ -1422,6 +1418,8 @@ export class SlickgridReact<TData = any> extends React.Component<SlickgridReactP
 
   /** Pre-Register any Resource that don't require SlickGrid to be instantiated (for example RxJS Resource) */
   protected preRegisterResources() {
+    this._registeredResources = this.gridOptions.externalResources || [];
+
     // bind & initialize all Components/Services that were tagged as enabled
     // register all services by executing their init method and providing them with the Grid object
     if (Array.isArray(this._registeredResources)) {