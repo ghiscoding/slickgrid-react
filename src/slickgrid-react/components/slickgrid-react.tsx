e29842aedf213c956cc309378bdae4d92ca5f8cc
Ghislain B <gbeaulac@gmail.com>
2023-12-20 22:32:13 -0500

    feat: reimplement highlight row, node-extend & fix few issues (#275)
    * feat: reimplement highlight row, node-extend & fix few issues

--- src/slickgrid-react/components/slickgrid-react.tsx
+++ src/slickgrid-react/components/slickgrid-react.tsx
@@ -49,12 +49,12 @@ import {
   // utilities
   autoAddEditorFormatterToColumnsWithEditor,
   emptyElement,
-  Utils as SlickUtils,
 } from '@slickgrid-universal/common';
 import { EventPubSubService } from '@slickgrid-universal/event-pub-sub';
 import { SlickFooterComponent } from '@slickgrid-universal/custom-footer-component';
 import { SlickEmptyWarningComponent } from '@slickgrid-universal/empty-warning-component';
 import { SlickPaginationComponent } from '@slickgrid-universal/pagination-component';
+import { extend } from '@slickgrid-universal/utils';
 
 import { dequal } from 'dequal/lite';
 import { Constants } from '../constants';
@@ -127,7 +127,7 @@ export class SlickgridReact<TData = any> extends React.Component<SlickgridReactP
     // if we already have grid options, when grid was already initialized, we'll merge with those options
     // else we'll merge with global grid options
     if (this.grid?.getOptions) {
-      mergedOptions = SlickUtils.extend<GridOption>(true, {} as GridOption, this.grid.getOptions() as GridOption, options as GridOption);
+      mergedOptions = extend<GridOption>(true, {} as GridOption, this.grid.getOptions() as GridOption, options as GridOption);
     } else {
       mergedOptions = this.mergeGridOptions(options);
     }
@@ -202,7 +202,7 @@ export class SlickgridReact<TData = any> extends React.Component<SlickgridReactP
     const prevDatasetLn = this._currentDatasetLength;
     const isDatasetEqual = dequal(newDataset, this.dataset || []);
     const isDeepCopyDataOnPageLoadEnabled = !!(this._gridOptions?.enableDeepCopyDatasetOnPageLoad);
-    let data = isDeepCopyDataOnPageLoadEnabled ? SlickUtils.extend(true, [], newDataset) : newDataset;
+    let data = isDeepCopyDataOnPageLoadEnabled ? extend(true, [], newDataset) : newDataset;
 
     // when Tree Data is enabled and we don't yet have the hierarchical dataset filled, we can force a convert+sort of the array
     if (this.grid && this.gridOptions?.enableTreeData && Array.isArray(newDataset) && (newDataset.length > 0 || newDataset.length !== prevDatasetLn || !isDatasetEqual)) {
@@ -1371,7 +1371,7 @@ export class SlickgridReact<TData = any> extends React.Component<SlickgridReactP
 
   protected mergeGridOptions(gridOptions: GridOption): GridOption {
     // use extend to deep merge & copy to avoid immutable properties being changed in GlobalGridOptions after a route change
-    const options = SlickUtils.extend(true, {}, GlobalGridOptions, gridOptions) as GridOption;
+    const options = extend(true, {}, GlobalGridOptions, gridOptions) as GridOption;
 
     options.gridId = this.props.gridId;
     options.gridContainerId = `slickGridContainer-${this.props.gridId}`;
