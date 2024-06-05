d5c4e9c369c87555eda4303b5a0fa43726131d3d
ghiscoding <gbeaulac@gmail.com>
2024-06-04 22:31:30 -0400

    feat: add optional Top-Header for Draggable Grouping & Header Grouping

--- src/examples/slickgrid/Example18.tsx
+++ src/examples/slickgrid/Example18.tsx
@@ -85,7 +85,7 @@ export default class Example18 extends React.Component<Props, State> {
   defineGrid() {
     const columnDefinitions: Column[] = [
       {
-        id: 'title', name: 'Title', field: 'title',
+        id: 'title', name: 'Title', field: 'title', columnGroup: 'Common Factor',
         width: 70, minWidth: 50,
         cssClass: 'cell-title',
         filterable: true,
@@ -101,7 +101,7 @@ export default class Example18 extends React.Component<Props, State> {
         }
       },
       {
-        id: 'duration', name: 'Duration', field: 'duration',
+        id: 'duration', name: 'Duration', field: 'duration', columnGroup: 'Common Factor',
         width: 70,
         sortable: true,
         filterable: true,
@@ -122,27 +122,8 @@ export default class Example18 extends React.Component<Props, State> {
         }
       },
       {
-        id: 'percentComplete', name: '% Complete', field: 'percentComplete',
-        minWidth: 70, width: 90,
-        formatter: Formatters.percentCompleteBar,
-        type: FieldType.number,
-        filterable: true,
-        filter: { model: Filters.compoundSlider },
-        sortable: true,
-        groupTotalsFormatter: GroupTotalFormatters.avgTotalsPercentage,
-        grouping: {
-          getter: 'percentComplete',
-          formatter: (g) => `% Complete: ${g.value}  <span class="text-primary">(${g.count} items)</span>`,
-          aggregators: [
-            new Aggregators.Sum('cost')
-          ],
-          aggregateCollapsed: false,
-          collapsed: false
-        },
-        params: { groupFormatterPrefix: '<i>Avg</i>: ' }
-      },
-      {
-        id: 'start', name: 'Start', field: 'start', minWidth: 60,
+        id: 'start', name: 'Start', field: 'start', columnGroup: 'Period',
+        minWidth: 60,
         sortable: true,
         filterable: true,
         filter: { model: Filters.compoundDate },
@@ -161,7 +142,7 @@ export default class Example18 extends React.Component<Props, State> {
         }
       },
       {
-        id: 'finish', name: 'Finish', field: 'finish',
+        id: 'finish', name: 'Finish', field: 'finish', columnGroup: 'Period',
         minWidth: 60,
         sortable: true,
         filterable: true,
@@ -181,7 +162,7 @@ export default class Example18 extends React.Component<Props, State> {
         }
       },
       {
-        id: 'cost', name: 'Cost', field: 'cost',
+        id: 'cost', name: 'Cost', field: 'cost', columnGroup: 'Analysis',
         width: 90,
         sortable: true,
         filterable: true,
@@ -201,7 +182,27 @@ export default class Example18 extends React.Component<Props, State> {
         }
       },
       {
-        id: 'effortDriven', name: 'Effort-Driven', field: 'effortDriven',
+        id: 'percentComplete', name: '% Complete', field: 'percentComplete', columnGroup: 'Analysis',
+        minWidth: 70, width: 90,
+        formatter: Formatters.percentCompleteBar,
+        type: FieldType.number,
+        filterable: true,
+        filter: { model: Filters.compoundSlider },
+        sortable: true,
+        groupTotalsFormatter: GroupTotalFormatters.avgTotalsPercentage,
+        grouping: {
+          getter: 'percentComplete',
+          formatter: (g) => `% Complete: ${g.value}  <span class="text-primary">(${g.count} items)</span>`,
+          aggregators: [
+            new Aggregators.Sum('cost')
+          ],
+          aggregateCollapsed: false,
+          collapsed: false
+        },
+        params: { groupFormatterPrefix: '<i>Avg</i>: ' }
+      },
+      {
+        id: 'effortDriven', name: 'Effort-Driven', field: 'effortDriven', columnGroup: 'Analysis',
         width: 80, minWidth: 20, maxWidth: 100,
         cssClass: 'cell-effort-driven',
         sortable: true,
@@ -228,9 +229,18 @@ export default class Example18 extends React.Component<Props, State> {
         rightPadding: 10
       },
       enableDraggableGrouping: true,
+
+      // pre-header will include our Header Grouping (i.e. "Common Factor")
+      // Draggable Grouping could be located in either the Pre-Header OR the new Top-Header
       createPreHeaderPanel: true,
       showPreHeaderPanel: true,
-      preHeaderPanelHeight: 40,
+      preHeaderPanelHeight: 30,
+
+      // when Top-Header is created, it will be used by the Draggable Grouping (otherwise the Pre-Header will be used)
+      createTopHeaderPanel: true,
+      showTopHeaderPanel: true,
+      topHeaderPanelHeight: 35,
+
       showCustomFooter: true,
       enableFiltering: true,
       // you could debounce/throttle the input text filter if you have lots of data
@@ -248,7 +258,9 @@ export default class Example18 extends React.Component<Props, State> {
       draggableGrouping: {
         dropPlaceHolderText: 'Drop a column header here to group by the column',
         // groupIconCssClass: 'mdi mdi-drag-vertical',
-        deleteIconCssClass: 'mdi mdi-close',
+        deleteIconCssClass: 'mdi mdi-close text-color-danger',
+        sortAscIconCssClass: 'mdi mdi-arrow-up',
+        sortDescIconCssClass: 'mdi mdi-arrow-down',
         onGroupChanged: (_e, args) => this.onGroupChanged(args),
         onExtensionRegistered: (extension) => this.draggableGroupingPlugin = extension,
       },
