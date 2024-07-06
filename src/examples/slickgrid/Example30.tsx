e4d90a0b5c4418b9d7dcf1f816e20eb7e8f7b3c7
Ghislain B <gbeaulac@gmail.com>
2024-07-05 20:39:39 -0400

    fix: Composite Editor should work with Cell Menu (#370)
    * fix: Composite Editor should work with Cell Menu

--- src/examples/slickgrid/Example30.tsx
+++ src/examples/slickgrid/Example30.tsx
@@ -455,6 +455,9 @@ export default class Example30 extends React.Component<Props, State> {
       },
       // when using the cellMenu, you can change some of the default options and all use some of the callback methods
       enableCellMenu: true,
+      cellMenu: {
+        activateCellOnMenuClick: true, // this is important to know which row index to use for Composite Editor
+      },
       gridMenu: {
         hideToggleDarkModeCommand: false, // hidden by default
         onCommand: (_, args) => {
