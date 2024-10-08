c53de1a6a6d36021db3026c692c31a2ec1b9e1bf
ghiscoding <gbeaulac@gmail.com>
2024-10-08 01:14:35 -0400

    chore: migrate demo from WebPack to ViteJS

--- test/jest-pretest.ts
+++
@@ -1,7 +0,0 @@
-import 'isomorphic-fetch';
-import 'jsdom-global/register';
-import Sortable from 'sortablejs';
-
-(global as any).navigator = { userAgent: 'node.js' };
-(global as any).Slick = (window as any).Slick = {};
-(global as any).Sortable = (window as any).Sortable = Sortable;
