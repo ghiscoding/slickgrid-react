5cebe958c4293f99e8a76475786590647be4246a
ghiscoding <gbeaulac@gmail.com>
2024-10-17 21:38:26 -0400

    chore: add optional props to load comp dynamically

--- src/slickgrid-react/services/reactUtils.ts
+++ src/slickgrid-react/services/reactUtils.ts
@@ -2,11 +2,11 @@ import React from "react";
 import { createRef } from "react";
 import { createRoot, type Root } from "react-dom/client";
 
-export function loadComponentDynamically<T = any>(customComponent: any, targetElm: HTMLElement): Promise<{ component: T, root: Root }> {
+export function loadComponentDynamically<T = any>(customComponent: any, targetElm: HTMLElement, props?: any): Promise<{ component: T, root: Root }> {
   return new Promise(resolve => {
     const compRef = createRef();
     const root = createRoot(targetElm);
-    root.render(React.createElement(customComponent, { ref: compRef }));
+    root.render(React.createElement(customComponent, { ...props, ref: compRef }));
 
     queueMicrotask(() => {
       resolve({ component: compRef.current as T, root });
