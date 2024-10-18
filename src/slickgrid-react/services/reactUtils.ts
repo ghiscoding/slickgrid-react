c41978b50e6b61575c5a0848315cd351b1f42c70
ghiscoding <gbeaulac@gmail.com>
2024-10-17 21:33:47 -0400

    chore: improve util arguments

--- src/slickgrid-react/services/reactUtils.ts
+++ src/slickgrid-react/services/reactUtils.ts
@@ -2,11 +2,10 @@ import React from "react";
 import { createRef } from "react";
 import { createRoot, type Root } from "react-dom/client";
 
-export function loadComponentDynamically<T = any>(customComponent: any, containerElm?: HTMLElement | null): Promise<{ component: T, root: Root }> {
+export function loadComponentDynamically<T = any>(customComponent: any, targetElm: HTMLElement): Promise<{ component: T, root: Root }> {
   return new Promise(resolve => {
     const compRef = createRef();
-    const target = containerElm ?? document.createElement('div');
-    const root = createRoot(target);
+    const root = createRoot(targetElm);
     root.render(React.createElement(customComponent, { ref: compRef }));
 
     queueMicrotask(() => {
