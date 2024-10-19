64dd7f3bd74984d4b3df84ae2c8a2eafe9231894
ghiscoding <ghiscoding@users.noreply.github.com>
2024-10-19 20:03:17 +0000

    deploy: 0816349aaed18a85eb3f0f620859912d554e85ee

--- assets/data/example-data.js
+++
@@ -1,2 +0,0 @@
-(()=>{const i=[];for(let e=0;e<500;e++){const t=i[e]={};t.id=e,t.title="Task "+e,t.description=`This is a sample task description.
-  It can be multiline`,t.duration="5 days",t.percentComplete=Math.round(Math.random()*100),t.start="01/01/2009",t.finish="01/05/2009",t.effortDriven=e%5===0}var a=i;})();
