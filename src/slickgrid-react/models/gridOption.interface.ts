37b81c999447d688ce262895b647d1b7b7911679
ghiscoding <gbeaulac@gmail.com>
2025-03-18 13:33:03 -0400

    chore: migrate Custom Pager to functional component - thanks to ChatGPT for the conversion

--- src/slickgrid-react/models/gridOption.interface.ts
+++ src/slickgrid-react/models/gridOption.interface.ts
@@ -1,11 +1,12 @@
 import type { BasePaginationComponent, BasePaginationModel, GridOption as UniversalGridOption } from '@slickgrid-universal/common';
 import type * as i18next from 'i18next';
+import type { ForwardRefExoticComponent, RefAttributes } from 'react';
 
 import type { RowDetailView } from './rowDetailView.interface';
 
 export interface GridOption extends UniversalGridOption {
   /** External Custom Pagination Component that can be provided by the user */
-  customPaginationComponent?: typeof BasePaginationComponent | (() => BasePaginationModel);
+  customPaginationComponent?: typeof BasePaginationComponent | (() => BasePaginationModel) | ForwardRefExoticComponent<any & RefAttributes<any>>;
 
   /** I18N translation service instance */
   i18n?: i18next.i18n;
